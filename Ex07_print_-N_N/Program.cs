0060f21532be1e69fb762f5662008a83d52ce1f5
AlexBulavin <bulavin1970@icloud.com>
2022-11-28 23:58:35 +0300

    Exercise 7 show numbers from -N to +N

---
+++ Ex07_print_-N_N/Program.cs
@@ -0,0 +1,11 @@
+﻿//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git  
+//Показать числа от -N до N
+
+Console.Write("\nВведите натуральное число :"); //Запрашиваем у пользователя 1 число. Проверку не производим (пока)
+int N = Convert.ToInt32(Console.ReadLine()); //Читаем и преобразуем строку в Int 32
+
+for (int i = - N; i <= N; i++)
+{
+
+    Console.WriteLine(i);
+}
