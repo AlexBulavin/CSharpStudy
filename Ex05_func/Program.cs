5adad707039dbb803dab712d09332d64138ae62b
AlexBulavin <bulavin1970@icloud.com>
2022-11-28 23:35:10 +0300

    Fifth exercise sin(a)

---
+++ Ex05_func/Program.cs
@@ -0,0 +1,8 @@
+﻿//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git  
+
+//Написать программу вычисления значения функции y = f(a)
+
+Console.Write("\nВведите аргумент для функции sin(a) в формате -360 ... 360 :"); //Запрашиваем у пользователя 1 число. Проверку не производим (пока)
+int y = Convert.ToInt32(Console.ReadLine()); //Читаем и преобразуем строку в double
+
+Console.Write("SIN(" + y.ToString() + ") = " + Math.Sin(y).ToString() + "\n");
