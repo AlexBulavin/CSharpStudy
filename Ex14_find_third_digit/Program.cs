333f4a47a8979ec2c11e97b0ec410be2baa2fcfe
AlexBulavin <bulavin1970@icloud.com>
2022-11-29 09:14:59 +0300

    Найти третью цифру числа или сообщить, что её нет

---
+++ Ex14_find_third_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git  
+
+Console.Write("\nВведите натуральное число : "); //Запрашиваем у пользователя 1 число. Проверку не производим (пока)
+int N = Convert.ToInt32(Console.ReadLine()); //Читаем и преобразуем строку в Int 32
+
+if (N >= 100) {
+    Console.WriteLine("Третья цифра числа " + N + " = " + N /100);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры в числе" + N + "нет");
+}
