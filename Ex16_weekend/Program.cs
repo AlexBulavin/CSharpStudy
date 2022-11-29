243baa53bd57573141d95ae8650148a99682f628
AlexBulavin <bulavin1970@icloud.com>
2022-11-29 09:34:11 +0300

    Дано число обозначающее день недели. Выяснить является номер дня недели выходным 1

--- Ex16_weekend/Program.cs
+++ Ex16_weekend/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+
 Console.Write("\nВведите номер дня недели: "); //Запрашиваем у пользователя 1 число. Проверку не производим (пока)
 int weekDay = Convert.ToInt32(Console.ReadLine()); //Читаем и преобразуем строку в int
 
