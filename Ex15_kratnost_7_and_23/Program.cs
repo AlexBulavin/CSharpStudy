31a8b28d4ae0bd6764ebc9d7e8ab1a01327699e1
AlexBulavin <bulavin1970@icloud.com>
2022-11-29 09:28:15 +0300

    Дано число. Проверить кратно ли оно 7 и 23

---
+++ Ex15_kratnost_7_and_23/Program.cs
@@ -0,0 +1,15 @@
+﻿//Дано число. Проверить кратно ли оно 7 и 23
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git  
+
+Console.Write("\nВведите число, которое проверяем на кратность: "); //Запрашиваем у пользователя 1 число. Проверку не производим (пока)
+int N = Convert.ToInt32(Console.ReadLine()); //Читаем и преобразуем строку в Int 32
+
+if (N >= 7 * 23) 
+{
+    if ((double)N/7 - N/7 > 0 | (double)N/23 - N/23 > 0)
+    {
+        Console.WriteLine("Первое число не кратно 7 и 23\n");
+    }
+    else Console.WriteLine("Число " + N + " кратно 7 и 23\n");
+}
+else {Console.WriteLine("Первое число не кратно 7 и 23\n");}
