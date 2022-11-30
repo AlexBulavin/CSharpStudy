a63a20ef8846b049146e615bc0aab626b5d23067
AlexBulavin <bulavin1970@icloud.com>
2022-11-30 23:41:35 +0300

    Digits sum in number

---
+++ Ex28_sum_digits_in_number/Program.cs
@@ -0,0 +1,11 @@
+﻿// Определить количество цифр в числе
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+Console.Write("\nВведите натуральное число N: ");
+string number = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < number.Length; i++)
+{
+    Console.WriteLine("number[" + i + "] = " + (int)Char.GetNumericValue(number[i]));
+    sum += (int)Char.GetNumericValue(number[i]);
+}
+Console.WriteLine("Сумма цифр в числе " + number + " = " + sum);
