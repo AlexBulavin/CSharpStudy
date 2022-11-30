621853927bea38fae1a926de02e4f546bdd427c2
AlexBulavin <bulavin1970@icloud.com>
2022-11-30 23:07:58 +0300

    A power B

---
+++ Ex26_A_power_B/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+Console.Write("\nВведите натуральное число N: ");
+double N = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nВведите степень M возведения числа N: ");
+int M = Convert.ToInt32(Console.ReadLine());
+double power = N;
+Console.WriteLine("");
+
+for (int i = 1; i < M; i++)
+{
+    power *=N;
+}
+Console.WriteLine(N + " в степени " + M + " = " + power + "\n");
