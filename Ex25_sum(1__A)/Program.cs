d8991c615d9c69aaf9cdb4fb804f8a70b0d5f2a1
AlexBulavin <bulavin1970@icloud.com>
2022-11-30 22:54:04 +0300

    Sum 1 - A

---
+++ Ex25_sum(1__A)/Program.cs
@@ -0,0 +1,12 @@
+﻿// See Найти сумму чисел от 1 до А
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+Console.Write("\nВведите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Console.WriteLine("");
+
+for (int i = 1; i <= N; i++)
+{
+    sum +=i;
+}
+Console.WriteLine("sum = " + sum + "\n");
