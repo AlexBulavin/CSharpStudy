88af9d7e419bcd000aed3b9b849c4c3ce9e3cbae
AlexBulavin <bulavin1970@icloud.com>
2022-11-30 21:49:45 +0300

    Polindrom

---
+++ Ex21_polindromom/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа проверяет пятизначное число на палиндромом
+////git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+Console.WriteLine("введите пятизначное число");
+string num = Console.ReadLine();
+if (num >= 10000 & nom <= 99999)
+{
+    string rev = string.Concat(num.Reverse());
+    Console.WriteLine(num == rev ? "палиндром" : "не палиндром");
+}
+
