105c8aeb0be32ee991b18e1b1a8f8006e19fb385
AlexBulavin <bulavin1970@icloud.com>
2022-11-30 23:15:22 +0300

    Digits in Number

---
+++ Ex27_digits_in_number/Program.cs
@@ -0,0 +1,5 @@
+﻿// Определить количество цифр в числе
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+Console.Write("\nВведите натуральное число N: ");
+int N = Console.ReadLine().Length;
+Console.WriteLine(N);
