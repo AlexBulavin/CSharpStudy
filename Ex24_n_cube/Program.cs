9c6689f6a00410777e4032b2f485e13e55224d63
AlexBulavin <bulavin1970@icloud.com>
2022-11-30 22:48:00 +0300

    N Cube

---
+++ Ex24_n_cube/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти кубы чисел от 1 до N
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+Console.Write("\nВведите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+for (int i = 1; i <= N; i++)
+{
+    Console.Write("|   " + i + "\t");
+}
+Console.WriteLine("");
+for (int i = 1; i <= N; i++)
+{
+    Console.Write("--------");
+}
+Console.WriteLine("");
+for (int i = 1; i <= N; i++)
+{
+    Console.Write("|   " + i*i*i + "\t");
+}
+Console.WriteLine("");
+Console.WriteLine("");
