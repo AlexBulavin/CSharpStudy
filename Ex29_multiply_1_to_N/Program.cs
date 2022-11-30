5d66d506962e4aa62accb3ecfb28a1999e1587ae
AlexBulavin <bulavin1970@icloud.com>
2022-11-30 23:53:59 +0300

    Написать программу вычисления произведения чисел от 1 до N

---
+++ Ex29_multiply_1_to_N/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+Console.Write("\nВведите натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int mult = 1;
+if (number > 1)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        mult *= i;
+    }
+    Console.WriteLine("Произведения чисел от 1 до " + number + " = " + mult);
+}
+else
+{
+    Console.WriteLine("Произведение чисел от 1 до " + number + " = 0");
+}
+
