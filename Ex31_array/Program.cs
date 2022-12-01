64672ff909029cc751286227a983701dcc474977
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 08:27:56 +0300

    Задать массив из 8 элементов и вывести их на экран

---
+++ Ex31_array/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задать массив из 8 элементов и вывести их на экран
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+
+int[] arr = new int[8]{0, 1, 2, 3, 4, 5, 6, 7};
+
+foreach(int i in arr)
+{
+    Console.Write($"{i} \t");
+}
+
+Console.WriteLine();
