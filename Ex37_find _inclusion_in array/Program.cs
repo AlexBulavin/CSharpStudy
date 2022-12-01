d7fb640434f6b53740000d575dc0b9e85fde6c3c
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 20:36:05 +0300

    В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]

---
+++ Ex37_find _inclusion_in array/Program.cs
@@ -0,0 +1,18 @@
+﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+
+int[] arr = new int[123];
+Random r = new Random();
+int count = 0;
+
+
+//Заполняем массив рандомными числами от 0 до 999
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = r.Next(0, 999);
+    if (arr[i] > 10 & arr[i] < 99) count ++;
+    Console.Write($"{arr[i]} \t");
+}
+Console.WriteLine();
+
+Console.Write("В массиве " + count + " чисел из диапазона 10, 99\n");
