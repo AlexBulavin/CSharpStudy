93c83f63898a769c95f2232641941a4055563ea8
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 23:40:31 +0300

    Opposit array

---
+++ Ex50_change_even_elements_to_opposit/Program.cs
@@ -0,0 +1,36 @@
+﻿//В двумерном массиве n×k заменить четные элементы на противоположные
+Console.Write("Введите размер массива nxk\n");
+int n = Convert.ToInt32(Console.ReadLine());
+int k = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Создаём массив " + n + " x " + k + "\n");
+
+int[,] arr = new int[n, k];
+Random r = new Random();
+
+//Заполняем массив рандомными числами от 0 до 99
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < k; j++)
+    {
+        arr[i, j] = r.Next(0, 200);
+        Console.Write($"{arr[i, j]} \t");
+    }
+    Console.WriteLine();
+    
+}
+Console.WriteLine("---------------------------------Проверяем массив на чётность и заменяем вместо чётных Arr[i, j] противоположные элементы массива Arr[n - i -1, k - j - 1] \n");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < k; j++)
+    {
+        if (arr[i, j] % 2 == 0)
+        {
+            arr[i, j] = arr[n-i-1, k-j-1];
+        };
+        Console.Write($"{arr[i, j]} \t");
+    }
+    Console.WriteLine();
+    
+}
