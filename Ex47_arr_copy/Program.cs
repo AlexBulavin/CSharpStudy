b1286eaf5788b2b839ff6ba4ab8b6292908461e9
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 23:13:23 +0300

    Написать программу копирования массива

---
+++ Ex47_arr_copy/Program.cs
@@ -0,0 +1,22 @@
+﻿//Написать программу копирования массива
+Console.Write("Создаём массив\n");
+
+int[] arr = new int[123];
+int[] arrCopy = new int[arr.Length];
+Random r = new Random();
+
+//Заполняем массив рандомными числами от 0 до 99
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = r.Next(0, 99);
+    Console.Write($"{arr[i]} \t");
+}
+Console.WriteLine("\n--------------------------------------------------------------- Выводим копию ------------------------------------------------------");
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arrCopy[i] = arr[i];
+    Console.Write($"{arrCopy[i]} \t");
+}
+
+Console.Write("\n");
