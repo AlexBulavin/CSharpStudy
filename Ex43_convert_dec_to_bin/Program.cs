1e9d3398bf7f15dd2108468deb76bb5f80ee2b66
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 22:14:37 +0300

    Dec to bin

---
+++ Ex43_convert_dec_to_bin/Program.cs
@@ -0,0 +1,4 @@
+﻿//Написать программу преобразования десятичного числа в двоичное
+Console.Write("Введите десятичное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число " + number + " в двоичной форме равно " + Convert.ToString(number, 2));
