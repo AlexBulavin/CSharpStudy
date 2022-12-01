84260d8070c32cf0464444d49006efbec0a60446
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 08:37:09 +0300

    Задан массив из 8 элементов, заполненных нулями и единицами, сделан вывод на экран

---
+++ Ex32_binary_array/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+
+int[] arr = new int[8]{0, 1, 1, 0, 1, 1, 1, 0};
+
+foreach(int i in arr)
+{
+    Console.Write($"{i} \t");
+}
+
+Console.WriteLine();
