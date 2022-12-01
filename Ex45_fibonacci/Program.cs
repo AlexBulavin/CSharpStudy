75fec9e997eb3dc69fe78430deebd0b04bff0c71
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 22:49:32 +0300

    Показать числа Фибоначчи

---
+++ Ex45_fibonacci/Program.cs
@@ -0,0 +1,16 @@
+﻿//Показать числа Фибоначчи
+Console.Write("Ищем 100 чисел Фибоначчи\n");
+ulong previous = 0;
+ulong current = 1;
+ulong next = 1;
+Console.WriteLine("Число " + previous);
+Console.WriteLine("Число " + current);
+for (int i = 2; i < 100; i++)
+{
+    next = previous + current;
+    previous = current;
+    current = next;
+    Console.WriteLine("Число " + next);
+}
+
+
