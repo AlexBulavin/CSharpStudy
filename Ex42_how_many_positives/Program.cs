343d81fc2bc85de51c84f75c3002be9595014740
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 22:04:31 +0300

    Определить сколько чисел больше 0 введено с клавиатуры

---
+++ Ex42_how_many_positives/Program.cs
@@ -0,0 +1,19 @@
+﻿//Определить сколько чисел больше 0 введено с клавиатуры
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+int number;
+int positive = 0;
+ConsoleKey key = default(ConsoleKey);
+
+while (true)
+{if(key != ConsoleKey.Q){
+Console.Write("\nВведите число ");
+number = Convert.ToInt32(Console.ReadLine());
+if (number > 0) 
+{
+    positive++;
+}
+Console.WriteLine("Количество положительных чисел = " + positive);
+} 
+else break;
+}
+Console.Write("\n");
