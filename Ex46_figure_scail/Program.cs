13c0303cc247f049500f4779aab277509ebaf415
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 23:07:28 +0300

    Написать программу масштабирования фигуры

---
+++ Ex46_figure_scail/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать программу масштабирования фигуры
+Console.Write("Введите координаты трёх точек. X1, Y1, X2, Y2, X3, Y3 = ");
+double[] startCoordinate = new double[6]{Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine())};
+Console.Write("Введите коэффициент мсштабирования = ");
+double k = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Новые координаты фигуры:");
+for (int i = 0; i < 6; i++)
+{
+    Console.Write(startCoordinate[i] * k + "\t");
+}
+Console.Write("\n");
