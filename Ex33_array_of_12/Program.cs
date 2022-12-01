121bb9f99437d12f7b9b918cc9e9a64bd421b531
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 08:53:41 +0300

    Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива

---
+++ Ex33_array_of_12/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+//git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+
+int[] arr = new int[12];
+Random r = new Random();
+int positive = 0;
+int negative = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = r.Next(-9, 9);
+    if (arr[i] < 0) negative ++;
+    else positive ++;
+    Console.Write($"{arr[i]} \t");
+}
+Console.WriteLine("\nКоличество положительных элементов = " + positive);
+Console.WriteLine("Количество отрицательных элементов = " + negative);
+Console.WriteLine();
