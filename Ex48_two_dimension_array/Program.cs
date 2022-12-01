73b94b1a21ae2ffe625f0bb4f5a12edc9321d612
AlexBulavin <bulavin1970@icloud.com>
2022-12-01 23:23:59 +0300

    Показать двумерный массив размером m×n заполненный целыми числами

---
+++ Ex48_two_dimension_array/Program.cs
@@ -0,0 +1,22 @@
+﻿//Показать двумерный массив размером m×n заполненный целыми числами
+Console.Write("Введите размер массива MxN\n");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Создаём массив " + m + " x " + n + "\n");
+
+int[,] arr = new int[m, n];
+Random r = new Random();
+
+//Заполняем массив рандомными числами от 0 до 99
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i, j] = r.Next(0, 99);
+        Console.Write($"{arr[i, j]} \t");
+    }
+    Console.WriteLine();
+    
+}
+
