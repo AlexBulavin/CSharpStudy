b15dd4a8df5a8f40bb51fdaa7800a1cfebbf9e72
AlexBulavin <bulavin1970@icloud.com>
2022-12-02 09:04:01 +0300

    Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.

---
+++ Ex57_order_every_row_of_array/Program.cs
@@ -0,0 +1,45 @@
+﻿//Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+Console.Write("Введите размер массива mxn\n");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Random r = new Random();
+
+Console.Write("Создаём массив " + m + " x " + n + "\n");
+
+int[,] arr = new int[m, n];
+int max;
+int exchange;
+
+//Заполняем массив случайными числами.
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i, j] = r.Next(0, 100);
+        Console.Write($"{arr[i, j]} \t");
+    }
+    Console.WriteLine();
+};
+
+Console.WriteLine("Сортируем элементы каждой строки массива:");
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {   
+        max = arr[i, j];
+        for (int k = j+1; k < n; k++)
+        {
+            if (arr[i, k] > max) 
+            {
+                exchange = max; 
+                max = arr[i, k];
+                arr[i, k] = exchange;
+            }
+        }
+        arr[i, j] = max; 
+        Console.Write($"{arr[i, j]} \t");
+    };
+    Console.WriteLine();
+};
+Console.WriteLine();
