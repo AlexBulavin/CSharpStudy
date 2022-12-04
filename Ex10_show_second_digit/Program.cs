864ba912b3cb9bca278fcf9cde094edb4065794f
AlexBulavin <bulavin1970@icloud.com>
2022-12-04 18:28:46 +0300

    Микро изменения в оформлении и проверки введённого пользователем числа

--- Ex10_show_second_digit/Program.cs
+++ Ex10_show_second_digit/Program.cs
@@ -1,13 +1,18 @@
 ﻿// Показать вторую цифру трёхзначного числа
 //git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git  
 
-Console.Write("\nВведите натуральное трёхзначное число :"); //Запрашиваем у пользователя 1 число. Проверку не производим (пока)
-int N = Convert.ToInt32(Console.ReadLine()); //Читаем и преобразуем строку в Int 32
+int N = 0;
+
+while (N < 100) 
+{
+    Console.Write("\nВведите натуральное трёхзначное число :"); //Запрашиваем у пользователя 1 число. Проверку не производим (пока)
+    N = Convert.ToInt32(Console.ReadLine()); //Читаем и преобразуем строку в Int 32
+}
 
 if (N >= 100) {
-      Console.WriteLine((N%100 - N%10)/10);
+      Console.WriteLine($"Вторая цифра числа {N} равна {(N%100 - N%10)/10}");
 }
 else 
 {
-    Console.WriteLine("N < 100");
+    Console.WriteLine($"Введенное число {N} < 100");
 }
