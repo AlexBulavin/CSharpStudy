448ec69ef86d3064fb77e31f0180c1adfc39eb56
AlexBulavin <bulavin1970@icloud.com>
2022-12-04 19:06:12 +0300

    Сделано на арифметике вместо стрингов

--- Ex21_polindromom/Program.cs
+++ Ex21_polindromom/Program.cs
@@ -1,10 +1,24 @@
 ﻿// Программа проверяет пятизначное число на палиндромом
+/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+СТРОКИ ИСПОЛЬЗОВАТЬ НЕЛЬЗЯ
+*/
 ////git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
-Console.WriteLine("введите пятизначное число");
-string num = Console.ReadLine();
-if (Convert.ToInt32(num) >= 10000 & Convert.ToInt32(num) <= 99999)
+int number = 0;
+while (number < 10000 | number > 99999) 
 {
-    string rev = string.Concat(num.Reverse());
-    Console.WriteLine(num == rev ? "палиндром" : "не палиндром");
+Console.Write("Введите пятизначное число:");
+number = Convert.ToInt32(Console.ReadLine());
 }
+int fiftForth = number/1000;
+// Console.WriteLine($"fiftForth = {fiftForth}");
+int firstSecond = (number%10)*10 + (number%100)/10;
+// Console.WriteLine($"firstSecond = {firstSecond}");
+    Console.WriteLine(fiftForth == firstSecond ? "палиндром" : "не палиндром");
 
