6796ca123622ba908151ada864eb926b1ff15f77
AlexBulavin <bulavin1970@icloud.com>
2022-12-04 18:49:12 +0300

    Микро изменения в оформлении и проверки введённого пользователем числа

--- Ex16_weekend/Program.cs
+++ Ex16_weekend/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
 //git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
-
-Console.Write("\nВведите номер дня недели: "); //Запрашиваем у пользователя 1 число. Проверку не производим (пока)
-int weekDay = Convert.ToInt32(Console.ReadLine()); //Читаем и преобразуем строку в int
-
+int weekDay = -1;
+while (weekDay < 1 | weekDay >7)
+{
+Console.Write("\nВведите номер дня недели от 1 (пнд) до 7 (воскр): "); //Запрашиваем у пользователя 1 число. Проверку не производим (пока)
+weekDay = Convert.ToInt32(Console.ReadLine()); //Читаем и преобразуем строку в int
+}
 Console.WriteLine(weekDay == 6 | weekDay == 7 ? "Выходной\n" : "Рабочий день\n");
