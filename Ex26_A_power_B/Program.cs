0e22f7f929ecc2a3ab14ccf50db58c9bf9ea6c11
AlexBulavin <bulavin1970@icloud.com>
2022-12-10 17:34:55 +0300

    Little WriteLine fix

--- Ex26_A_power_B/Program.cs
+++ Ex26_A_power_B/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Возведите число А в натуральную степень B используя цикл
 //git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+using static System.Console;
+
 Console.Write("\nВведите натуральное число N: ");
 double N = Convert.ToDouble(Console.ReadLine());
 Console.Write("\nВведите степень M возведения числа N: ");
@@ -9,6 +11,6 @@ Console.WriteLine("");
 
 for (int i = 1; i < M; i++)
 {
-    power *=N;
+    power *= N;
 }
-Console.WriteLine(N + " в степени " + M + " = " + power + "\n");
+WriteLine($"{N} в степени {M} = {power.ToString(" ### ### ### ### ### ### ### ### ### ### ### ### ###")}\n");
