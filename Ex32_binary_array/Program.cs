4a26d9d2e0a454ca481269f95bc34f805378cc99
AlexBulavin <bulavin1970@icloud.com>
2022-12-11 09:06:51 +0300

    Исправлено отображение в консоли для многозначного показателя степени

--- Ex32_binary_array/Program.cs
+++ Ex32_binary_array/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов (из диапазона [0-14] ) и выводит на экран десятичное представление числа, записанного в СС по основанию 15
-N: 3 [9, 4, 12] => 2097
+countingSys: 3 [9, 4, 12] => 2097
 Для проверки можно использовать https://numsys.ru/convert/2097/10/15
 */
 //git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
@@ -7,7 +7,10 @@ using static MyLibrary123;
 using static System.Console;
 
 Write($"Введите размерность массива: ");
-int M = Convert.ToInt32(Console.ReadLine());
+string dimBase = Console.ReadLine();
+int arrayDimension = Convert.ToInt32(dimBase);
+int dimDigit, singlePowerDigit;
+char[] arrDim = new char[dimBase.Length];
 WriteLine();
 Write($"Введите основание системы счисления: ");
 string osBase = Console.ReadLine();
@@ -16,22 +19,32 @@ for (int i = 0; i < os.Length; i++)
 {
     os[i] = CharSelector((int)Char.GetNumericValue(osBase[i]), 0);//Конвертировали основние системы счисления в int и вызвали метод из библиотеки для преобразования через Unicode в подстрочный индекс
 }
-WriteLine();
-int N = Convert.ToInt32(osBase);
-
-int[] arr = new int[M];
+//WriteLine(String.Join("***", os));
+int countingSys = Convert.ToInt32(osBase);
+//WriteLine($"arrayDimension = {arrayDimension}\nN = {countingSys}");
+int[] arr = new int[arrayDimension];
 long res = 0;
 long temp = 0;
 
-for (int i = 0; i < M; i++)
+for (int i = 0; i < arrayDimension; i++)
 {
-    arr[i] = Random.Shared.Next(0, N);
-    temp = (long)(arr[i] * Math.Pow(N, M - i));
-    Write($"{arr[i]}*{N}{CharSelector(M - i, 1)}");
-    Write(i < M - 1 ? " + " : "\n");
+    //Очистить массив
+    Array.Clear(arrDim, 0, arrDim.Length);
+    singlePowerDigit = (int)Math.Log(arrayDimension - i, 10) + 1;//Количество цифр в показателе степени
+    //WriteLine($"singlePowerDigit = {singlePowerDigit}");
+    for (int j = 0; j < singlePowerDigit; j++)
+    {
+        dimDigit = (arrayDimension-i-1)%(int)Math.Pow(10, singlePowerDigit - j)/(int)Math.Pow(10, singlePowerDigit - j - 1);//Извлекаем значение цифры из показателя степени текущего слагаемого (элемента массива)
+        arrDim[j] = CharSelector(dimDigit, 1);//Вызвали метод из библиотеки для преобразования через Unicode в подстрочный индекс
+    }
+    //Заполняем массив рандомными значениями
+    arr[i] = Random.Shared.Next(0, countingSys);
+    temp = (long)(arr[i] * Math.Pow(countingSys, arrayDimension - i - 1));//temp = (long)(arr[i] * Math.Pow(countingSys, arrayDimension - i - 1));
+    Write($"{arr[i]}*{countingSys}{String.Join("", arrDim)}");//Выводим в консоль разложение числа по выбранному основанию
+    Write(i < arrayDimension - 1 ? " + " : "\n");
     res += temp;
 }
 
-WriteLine($"{String.Join("", arr)}{String.Join("", os)} = {res}\u2081\u2080\n");//Код \u2081\u2080 соответствует символам ₁₀
+WriteLine($"{String.Join("", arr)}{String.Join("", os)} = {res}\u2081\u2080\n");//Выводим в консоль результат в виде 1012333₁₅ = 547473₁₀ Код \u2081\u2080 соответствует символам ₁₀
 WriteLine();
 
