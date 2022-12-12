5f4b4a9405601bf91a9040bc6c7620e04eb69338
AlexBulavin <bulavin1970@icloud.com>
2022-12-13 00:24:16 +0300

    Рефакторинг через методы

--- Ex36_3_digit_even_odd_array/Program.cs
+++ Ex36_3_digit_even_odd_array/Program.cs
@@ -1,22 +1,26 @@
 ﻿//Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
 //git push https://ghp_bA2****************3vtX0M@github.com/AlexBulavin/CSharpStudy.git
+using static MyLibrary123;
+using static System.Console;
 
-int[] arr = new int[50];
-Random r = new Random();
-int even_count = 0;
-int odd_count = 0;
+bool DEBUG = false;
+// 0. Задать массив
+// 1. заполнить случайными положительными трёхзначными числами
+// 2. Показать количество нечетных\четных чисел
 
-//Заполняем массив рандомными числами от 100 до 999
-for (int i = 0; i < arr.Length; i++)
-{
-    arr[i] = r.Next(100, 999);
-    if (arr[i] % 2 == 0)
-    {even_count++;}
-    else
-    {odd_count++;};
-    Console.Write($"{arr[i]} \t");
-}
-Console.WriteLine();
+// 0. Задать массив
+int[] arr = CreateArrayInt(Input("\nЗадайте размерность массива: "));
+int[] count = CreateArrayInt(2);//Чётные числа, Нечётные числа
 
-Console.Write("В массиве " + even_count + " чётных и " + odd_count + " нечётных чисел\n");
+// 1. заполнить случайными положительными трёхзначными числами
+FillCheckPrintRandInt(arr, count, DEBUG);
+if (DEBUG) WriteLine($"Line 16 {count[0]} {count[1]}");
+WriteLine();
 
+//1.1 Распечатать массив
+WriteLine(PrintGood(arr, 1));
+WriteLine();
+
+// 2. Показать количество нечетных/четных чисел
+Write($"В массиве {count[0]} чётных и {count[1]} нечётных чисел\n");
+WriteLine();
