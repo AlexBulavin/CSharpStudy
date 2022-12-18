4f03ae0dc38e44e569fcd8a0b953fd6adbc76dbf
AlexBulavin <bulavin1970@icloud.com>
2022-12-19 00:29:27 +0300

    Refactor with methods

--- Ex53_find_element_in_array/Program.cs
+++ Ex53_find_element_in_array/Program.cs
@@ -1,27 +1,19 @@
 ﻿// В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет
-Console.Write("Введите размер массива mxn\n");
-int m = Convert.ToInt32(Console.ReadLine());
-int n = Convert.ToInt32(Console.ReadLine());
+using static MyLibrary;
+using static System.Console;
+Console.Clear();
+
+int m = Input("Введите количество строк двумерного массива: ");
+int n = Input("Введите количество столбцов двумерного массива: ");
+int[,] arr = CreateArrayIntTwo(m, n);
 bool trigger = false;
-Random r = new Random();
 
-Console.Write("Создаём массив " + m + " x " + n + "\n");
-
-int[,] arr = new int[m, n];
 
 //Заполняем массив случайными числами. Если i и j чётные, то квадратами.
-for (int i = 0; i < m; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        arr[i, j] = r.Next(5, 20);
-        Console.Write($"{arr[i, j]} \t");
-    }
-    Console.WriteLine();
-};
+FillRandIntTwo(arr, 10, true, 99, true);
+Write(PrintGoodTwo(arr,1));
 
-Console.Write("Задайте число, которое нужно найти в массиве:");
-int number = Convert.ToInt32(Console.ReadLine());
+int number = Input("Задайте число, которое нужно найти в массиве:");
 
 for (int i = 0; i < m; i++)
 {
@@ -29,12 +21,13 @@ for (int i = 0; i < m; i++)
     {
         if (arr[i, j] == number)
         {
-            Console.Write($"Элемент {arr[i, j]} найден, имеет индексы [{i}, {j}] \n");
+            WriteLine($"Самый первый встретившийся элемент {arr[i, j]} найден, имеет индексы [{i}, {j}] \n");
             trigger = true;
             break;
+            
         }   
         if (trigger) break;
     }   
     if (trigger) break;
 }
-if (!trigger) Console.WriteLine($"\nЭлемент {number} не найден\n");
+if (!trigger) WriteLine($"\nЭлемент {number} не найден\n");
