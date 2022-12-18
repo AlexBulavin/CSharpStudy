66de33f27ac8c513fc989613656b7ba7c1c098f4
AlexBulavin <bulavin1970@icloud.com>
2022-12-18 23:55:07 +0300

    Refactoring with Methods

--- Ex44_cross_lines/Program.cs
+++ Ex44_cross_lines/Program.cs
@@ -1,13 +1,12 @@
 ﻿//Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
-Console.Write("Введите коэффициенты первой прямой. k1 = ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("b1 = ");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите коэффициенты второй прямой. k2 = ");
-double k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("b2 = ");
-double b2 = Convert.ToDouble(Console.ReadLine());
+using static System.Console;
+using static MyLibrary;
+
+double k1 = Input("Введите коэффициенты первой прямой. k1 = ", true);
+double b1 = Input("b1 = ", true);
+double k2 = Input("Введите коэффициенты второй прямой. k2 = ", true);
+double b2 = Input("b2 = ", true);
 double y = (b2-b1)/(k1-k2);
 double x = (y-b1)/k1;
 
-Console.WriteLine("Координаты пересечения Х = " + x + " Y = " + y);
+WriteLine($"\nКоординаты пересечения Х = {x} Y = {y}\n");
