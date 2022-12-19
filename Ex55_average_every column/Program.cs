3c466ee38a87649c0f31d8423c4af62725dc4285
AlexBulavin <bulavin1970@icloud.com>
2022-12-19 09:38:16 +0300

    Cosmetic decor improvements

--- Ex55_average_every column/Program.cs
+++ Ex55_average_every column/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
 using static MyLibrary;
 using static System.Console;
-Console.Clear();
+Console.Clear();//Очищаем консоль
 bool DEBUG = false;//true;//
 int n = Input("Введите количество строк двумерного массива: ");
 int m = Input("Введите количество столбцов двумерного массива: ");
@@ -9,10 +9,11 @@ int m = Input("Введите количество столбцов двумер
 int[,] arr = CreateArrayIntTwo(n, m);
 float[] average = CreateArrayFloat(m);
 
-//Заполняем массив случайными числами.
+//Заполняем массив случайными числами и вычисляем среднее значение.
 FillRandIntTwo(arr, 0, true, 100, true);
-Write(PrintGoodTwo(arr, 1));
 
+//Выводим полученный массив на печать
+Write(PrintGoodTwo(arr, 1));
 WriteLine("Средние значения по столбцам:");
 
 for (int i = 0; i < m; i++)//Идём по столбцам
@@ -20,12 +21,12 @@ for (int i = 0; i < m; i++)//Идём по столбцам
     for (int j = 0; j < n; j++)//Идём по строкам
         {
         if (DEBUG) WriteLine($" n (кол-во строк) = {n}, m (кол-во столбцов) = {m}, i = {i}, j = {j}, arr[i,j] = {arr[i,j]}, average[i] = {average[i]}");
-        average[i] += arr[j, i];
+        average[i] += arr[j, i]; //Суммируем элементы по столбцу
         }
-    average[i] = average[i]/n;
-    
+    average[i] = average[i]/n;//Вычисляем среднее значение
 }
+//Выводим средние значения в заданном цвете
+ChangeForegroundColor(4);
 Write(PrintGood(average, 1));
+ResetColor();
 WriteLine();
-
-
