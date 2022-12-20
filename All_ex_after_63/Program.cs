1c096b16eee58523f7af900bcd993791f726e00d
AlexBulavin <bulavin1970@icloud.com>
2022-12-20 09:56:48 +0300

    Print 1 - N and N - 1 numbers

--- All_ex_after_63/Program.cs
+++ All_ex_after_63/Program.cs
@@ -3,4 +3,5 @@
 
 //Seminar_17_12_2022.Task1();
 //Spyral.Matrix1();
-StringArrayParser.Parser1();
+//StringArrayParser.Parser1();
+Recursion.Natural1NPrint();
---
+++ All_ex_after_63/Recursion.cs
@@ -0,0 +1,35 @@
+/*Показать натуральные числа от 1 до N, N задано */
+using static MyLibrary;
+using static System.Console;
+public class Recursion
+{
+    public static void Natural1NPrint()
+    {
+        Console.Clear();//Очищаем консоль
+        bool DEBUG = false;//true;// Задаём режим работы - дебаг или прод 
+        int n = Input("Введите натуральное число N: ");
+        string output = String.Empty;
+
+        //Выводим через рекурсию.
+        ChangeForegroundColor(4);
+        WriteLine($"{NaturalRayOpposit(1,n)}");
+        WriteLine($"{NaturalRay(n)}");
+        Console.ResetColor();
+    }
+
+    public static string NaturalRay(int N)
+    {
+        string output = String.Empty;
+        if (N > 0) output += $"{N} {NaturalRay(N - 1)}";
+
+        return output;
+    }
+
+    public static string NaturalRayOpposit(int count, int N)
+    {
+        string output = String.Empty;
+        if (count <= N) output += $"{count} {NaturalRayOpposit(count + 1, N)}";
+
+        return output;
+    }
+}
