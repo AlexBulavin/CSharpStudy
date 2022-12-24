7445e7b4ae746f44b213a02f77e33353a07409b9
AlexBulavin <bulavin1970@icloud.com>
2022-12-24 08:06:44 +0300

    Fixes

--- All_ex_after_63/Data_Info_Arr_75.cs
+++ All_ex_after_63/Data_Info_Arr_75.cs
@@ -18,9 +18,7 @@ public class DataInfoArr
         DateTime startTime = DateTime.Now;
         Console.Clear();//Очищаем консоль
         bool DEBUG = false;//true;// Задаём режим работы - дебаг или прод 
-        int shift = 0;
-        int result = 0;
-        int exponent = 0;
+
         //Нужно переписать это на preset data или делать динамическим массивом
         int[] data;
         int[] info;
@@ -42,64 +40,36 @@ public class DataInfoArr
             if (DEBUG) WriteLine($"Используем предустановленные данные");
             data = new int[] { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
             info = new int[] { 2, 3, 3, 1 };
-
+            PrintOutResult(DEBUG, data, info);
         }
         else if (selectDataGenerate == 2)
         {
-            //Создаём массивы автоматическим рандомным от 3 до 20
+            //2. Создаём массивы автоматическим рандомным от 3 до 20
             int[] sourceData = CreateArrayInt(Random.Shared.Next(3, 21));
             FillRandInt(sourceData, 0, true, 100, true);
-            string sourceDataBin = String.Empty;
-            string infoString = String.Empty;
-            int binCounter;
-            int restOfDeviding, leftPartOfDeviding;
-            //Конвертируем значения массива в двоичный код и узнаём количество символов в двоичном представлении
-            for (int i = 0; i < sourceData.Length; i++)
-            {
-                restOfDeviding = sourceData[i] / 2;
-                leftPartOfDeviding = sourceData[i] % 2;
-                binCounter = 1;//Показатель степени для числа в двоичном представлении, то есть элемент массива info
-                while (restOfDeviding >= 2)
-                {
-                    sourceDataBin += leftPartOfDeviding;
-                    binCounter++;
-                }
-                infoString += binCounter;//Строка с данными по всем длинам двоичных элементов
-            }
-            int sourceDataBinLength = sourceDataBin.Length;
-            int infoStringLength = infoString.Length;
-            //Заполняем массивы data и info
-            data = new int[sourceDataBinLength];
-            info = new int[infoStringLength];
-            for (int i = sourceDataBin.Length - 1; i >= 0; i--)
-            {
-                data[sourceDataBinLength - i + 1] = sourceDataBin[i];
-            }
-            for(int i = infoStringLength; i >= 0; i--)
-            {
-                info[infoStringLength - i + 1] = infoString[i];
-            }
+            autoDataAndInfoArrFilling(DEBUG, out data, out info, sourceData);
         }
         else if (selectDataGenerate == 3)
         {
+            //3. Создать метод ручного ввода.
             int[] sourceData = new int[Input("Укажите размер массива данных")];
             for (int i = 0; i < sourceData.Length; i++) sourceData[i] = Input($"sourceData[{i}] = ");
-
+            autoDataAndInfoArrFilling(DEBUG, out data, out info, sourceData);
         }
-        //Конвертируем значения массива в двоичный код и узнаём количество символов в двоичном представлении
-
-        //Заполняем массивы data и info
-        //Печатаем исходные массивы
-        Write($"data array:");
-        SetCursorPosition(15, 0);
-        WriteLine(PrintGood(data, 1));
-
-        Write($"info array:");
-        SetCursorPosition(15, 1);
-        WriteLine(PrintGood(info, 1));
-        WriteLine();
 
         //Выводим получившиеся данные через цикл //рекурсию.
+        // PrintOutResult(DEBUG, ref shift, ref result, ref exponent, out data, out info);
+        WriteLine("\n");
+        ResetColor();
+        ProgramMonitoring(startTime, DEBUG);
+        WriteLine();
+    }
+
+    private static void PrintOutResult(bool DEBUG, int[] data, int[] info)
+    {
+        int shift = 0;
+        int exponent = 0;
+        int result = 0;
         for (int i = 0; i < info.Length; i++)
         {
             for (int j = shift; j < info[i] + shift; j++)
@@ -120,21 +90,69 @@ public class DataInfoArr
             shift += info[i];
             result = 0;
         }
-        WriteLine("\n");
-        ResetColor();
-        ProgramMonitoring(startTime, DEBUG);
+    }
+
+    private static void dataAndInfoArrFilling(bool DEBUG, out int[] data, out int[] info, string sourceDataBin, string infoString, int sourceDataBinLength, int infoStringLength)
+    {
+        //Заполняем массивы data и info
+        data = new int[sourceDataBinLength];
+        info = new int[infoStringLength];
+        for (int i = sourceDataBin.Length - 1; i >= 0; i--)
+        {
+            data[sourceDataBinLength - i + 1] = sourceDataBin[i];
+        }
+        for (int i = infoStringLength; i >= 0; i--)
+        {
+            info[infoStringLength - i + 1] = infoString[i];
+        }
+        PrintOutResult(DEBUG, data, info);
+        printDataAndInfoArr(data, info);
+    }
+    private static void printDataAndInfoArr(int[] data, int[] info)
+    {
+        //Заполняем массивы data и info
+        //Печатаем исходные массивы
+        Write($"data array:");
+        SetCursorPosition(15, 0);
+        WriteLine(PrintGood(data, 1));
+
+        Write($"info array:");
+        SetCursorPosition(15, 1);
+        WriteLine(PrintGood(info, 1));
         WriteLine();
     }
-    /*data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
-      info = {2, 3, 3, 1 }
-    выходные данные:
-    1, 7, 0, 1*/
 
-    public static void DataInfoArrFill()
+    private static void autoDataAndInfoArrFilling(bool DEBUG, out int[] data, out int[] info, int[] sourceData)
     {
-        //Заполняем массивы data и info в соответствии с массивом sourceData
-
+        string sourceDataBin = String.Empty;
+        string infoString = String.Empty;
+        int binCounter;
+        int restOfDeviding, leftPartOfDeviding;
+        //Конвертируем значения массива в двоичный код и узнаём количество символов в двоичном представлении
+        for (int i = 0; i < sourceData.Length; i++)
+        {
+            restOfDeviding = sourceData[i] / 2;
+            leftPartOfDeviding = sourceData[i] % 2;
+            binCounter = 1;//Показатель степени для числа в двоичном представлении, то есть элемент массива info
+            while (restOfDeviding >= 2)
+            {
+                sourceDataBin += leftPartOfDeviding;
+                binCounter++;
+            }
+            infoString += binCounter;//Строка с данными по всем длинам двоичных элементов
+        }
+        int sourceDataBinLength = sourceDataBin.Length;
+        int infoStringLength = infoString.Length;
+        dataAndInfoArrFilling(DEBUG, out data, out info, sourceDataBin, infoString, sourceDataBinLength, infoStringLength);
     }
+
+
+
+    /*data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+ info = {2, 3, 3, 1 }
+выходные данные:
+1, 7, 0, 1*/
+
     // public static void RecursionPermutationWords(string baseArr, string w, int n, bool DEBUG)
     // {
     //     if (DEBUG)
