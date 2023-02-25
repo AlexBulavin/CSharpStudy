b1ba4e848ae0eaf7e772288aaf30f01afd28801f
AlexBulavin <bulavin1970@icloud.com>
2023-02-25 13:04:26 +0300

    Add about.rtml and enharitage it from the base.html

--- FLASKBOOTCAMP/lesson24flask.py
+++ FLASKBOOTCAMP/lesson24flask.py
@@ -3,24 +3,25 @@ from random import randint as rd
 
 app = Flask(__name__)
 
-file = open('file.txt', 'r', encoding='utf-8')
-
-resultData = list()
-
-for line in file.readlines():
-    resultData.append(tuple(line.split('\n')[0].split(';')))
-
-file.close()
 
 @app.route('/')
 def main_page():
-    name = resultData
+    with open('file.txt', 'r', encoding='utf-8') as file:
+        resultData = list()
+        for line in file.readlines():
+            resultData.append(tuple(line.split('\n')[0].split(';')))
     game = rd(0, 1)
-    return render_template('base.html', name=name, game=game) #Делаем переадресацию главной страницы на указанный в скобках файл
+    return render_template('base.html', data=resultData, game=game) #Делаем переадресацию главной страницы на указанный в скобках файл
 
 @app.route('/index/<x>/<y>')
 def index_page(x,y):
     return f'<h2>Index page</h2> {int(x) + int(y)}'
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 
 if __name__ == '__main__':
     app.run()
